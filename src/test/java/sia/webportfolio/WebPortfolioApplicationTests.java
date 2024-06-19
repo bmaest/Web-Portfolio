19220060791f48d94d0c306c6b0fe5b54a648c3a
bmaes <bmaest5@wgu.edu>
2024-06-19 12:14:01 -0700

    Created Project

---
+++ src/test/java/sia/webportfolio/WebPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package sia.webportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
